606b1e85e786ca2463782294f2dedbac9da2c274
Shubhra Dwivedi <shubhradwivedi21@gmail.com>
2025-03-09 23:10:11 +0530

    changed the router

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider as ChakraProvider } from "./components/ui/provider";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import RecipeProvider from "./Context/RecipeProvider";
 import AuthProvider from "./Context/AuthProvider";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
     <RecipeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <ChakraProvider>
           <App />
         </ChakraProvider>
-      </BrowserRouter>
+      </HashRouter>
     </RecipeProvider>
     </AuthProvider>
   </StrictMode>
